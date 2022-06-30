c840b97f35ed29e4a3e5b3f5ef938d6344f04744
aadeye <aadeyemi@cloudhight.com>
2022-06-30 13:14:13 -0400

    Added Variables

--- route53.tf
+++ route53.tf
@@ -1,13 +1,13 @@
 # Route 53 Hosted Zone
 resource "aws_route53_zone" "elizabethfolzgroup_zone" {
-  name          = "www.elizabethfolzgroup.com"
+  name          = var.domain_name
   force_destroy = true
 }
 
 # Route 53 A Record
 resource "aws_route53_record" "ElizabethFolzGroup_Website" {
   zone_id = aws_route53_zone.elizabethfolzgroup_zone.zone_id
-  name    = "www.elizabethfolzgroup.com"
+  name    = var.domain_name
   type    = "A"
   alias {
     name                   = aws_lb.elizabethfolzgroup-elb.dns_name
